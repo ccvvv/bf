6e6a922239c75d4e1a61f56e8af0e0ada6e5fc6e
neoiso <178282609@qq.com>
2019-09-04 01:43:08 +0800

    add the code

---
+++ skip2.c
@@ -0,0 +1,14 @@
+//P96 4.17
+#include<stdio.h>
+int main(void)
+{
+	int n;
+
+	printf("Please enter three integers:\n");
+	scanf("%*d %*d %d",&n);
+	printf("The last integer was %d\n", n);
+
+	return 0;
+}
+
+
